4f9318c7564a57985ea9f0777404a1144610ef4c
Maheshh Thakor <156879292+Maheshh-03@users.noreply.github.com>
2026-04-14 18:44:29 +0530

    Create testbot.php for sending Telegram messages
    Added a basic Telegram bot script to send a test message.
    
    Signed-off-by: Maheshh Thakor <156879292+Maheshh-03@users.noreply.github.com>

--- testbot.php
+++ testbot.php
@@ -1,1 +1,25 @@
 
+<?php
+$botToken = 8701156230:AAFFl_ppc4_9eXw7BJ8BWmZklJ8LpVte-gI; // No quotes around token
+$chatId = 8701156230;     // Just the number
+
+$url = "https://api.telegram.org/bot$botToken/sendMessage";
+$data = [
+    'chat_id' => $chatId,
+    'text' => '🧪 TEST MESSAGE - Bot is ALIVE!',
+    'parse_mode' => 'HTML'
+];
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "HTTP: $httpCode\n";
+echo "Response: $response\n";
+?>
